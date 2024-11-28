bc9f9dfe46cd70c8690d8425abfa901ad91be269
Marco Casanova <m.casanova.dev@gmail.com>
2024-11-28 02:58:38 +0100

    handle auth

---
+++ src/middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+const authenticate = (req, res, next) => {
+    //const token = req.header('Authorization');
+    const token = req.cookies?.token;
+
+    if (!token) return res.status(401).json({ message: "No token, authorization denied" });
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        res.status(401).json({ message: "Token is not valid" });
+    }
+};
+
+const authorize = (role) => (req, res, next) => {
+    if (req.user.role !== role) return res.status(403).json({ message: "Forbidden" });
+    next();
+};
+
+module.exports = { authenticate, authorize };
